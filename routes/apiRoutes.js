2b550e8effaa7dae011c5bdffc68cdfd28cd3196
AG-gash <74515756+ashley-ag@users.nereply.github.com>
2021-05-20 19:28:47 -0700

    pushing data

---
+++ routes/apiRoutes.js
@@ -0,0 +1,30 @@
+let note = require("../db/db.json");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+module.exports = (app) => {
+  app.get("/api/notes", (req, res) => {
+    res.json(note);
+  });
+
+  app.post("/api/notes", (req, res) => {
+    const data = req.body;
+    data.id = uuidv4(data.id);
+    note.push(data);
+    fs.writeFile("db/db.json", JSON.stringify(note), (err) => {
+      if (err) throw err;
+    });
+    res.json(true);
+  });
+
+  app.delete("/api/notes/:id", (req, res) => {
+    const noteID = req.params.id;
+    note = note.filter((notes, index) => {
+      return noteID !== notes.id;
+    });
+    fs.writeFile("db/db.json", JSON.stringify(note), (err) => {
+      if (err) throw err;
+    });
+    res.json(true);
+  });
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+// dependencies
+const express = require("express");
+
+// setting up express
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+// setting up express for data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// requiring routes - separate folders for cleaner look
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//listener + console log
+app.listen(PORT, () => {
+  console.log("App listening on PORT: " + PORT);
+});
