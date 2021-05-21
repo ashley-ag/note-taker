71f8f8132a1741a4d28591f6c3033c529b627c58
AG-gash <74515756+ashley-ag@users.nereply.github.com>
2021-05-20 19:44:53 -0700

    small changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,12 @@
 const path = require("path");
 
 module.exports = (app) => {
+  // GET html files
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/assets/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/assets/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 };
